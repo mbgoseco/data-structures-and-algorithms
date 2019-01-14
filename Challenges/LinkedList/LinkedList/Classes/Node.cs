f4ce94ffc684f8cc4814b03213d5e463ac883b8d
mbgoseco <mbgoseco@gmail.com>
2019-01-13 21:57:25 -0800

    methods complete

---
+++ Challenges/LinkedList/LinkedList/Classes/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedLists.Classes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+                Value = value;
+        }
+
+    }
+}
