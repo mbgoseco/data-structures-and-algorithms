905b61feab80802efa269f302605be4cc169ee87
mbgoseco <mbgoseco@gmail.com>
2019-01-21 01:16:18 -0800

    class members added

---
+++ Data-Structures/StacksAndQueues/XUnitTestSnQ/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using StacksAndQueues;
+using StacksAndQueues.Classes;
+
+namespace XUnitTestSnQ
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
