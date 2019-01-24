7b9ab110079fc7250bd4a29eb7d5ad661a48e356
mbgoseco <mbgoseco@gmail.com>
2019-01-24 11:45:53 -0800

    methods complete

---
+++ Challenges/AnimalShelter/AnimalShelter/Classes/AnimalShelter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StacksAndQueues.Classes;
+
+namespace AnimalShelter.Classes
+{
+    public class AnimalShelter
+    {
+        public Animal Front { get; set; }
+        public Animal Rear { get; set; }
+        public AnimalShelter Queue { get; set; }
+
+        public AnimalShelter(Animal node)
+        {
+            Queue = new AnimalShelter
+            {
+                Front = node,
+                Rear = node
+            };
+        }
+
+        public AnimalShelter()
+        {
+
+        }
+
+        public void Enqueue(string value)
+        {
+            Animal animal = new Animal(value);
+            Rear.Next = animal;
+            Rear = animal;
+        }
+
+        public Animal Dequeue(Animal pref)
+        {
+            if (pref.Value.ToLower() != "cat" || pref.Value.ToLower() != "dog")
+            {
+                return null;
+            }
+            if (Queue.Front.Value == pref.Value)
+            {
+                Animal temp = Queue.Front;
+                Queue.Front = Queue.Front.Next;
+                temp.Next = null;
+                return temp;
+            }
+
+            AnimalShelter tempQueue = new AnimalShelter();
+
+            while (Queue.Front.Next != null)
+            {
+                if (Queue.Front.Value == pref.Value)
+                {
+                    Animal firstTemp = Queue.Front;
+                    Queue.Front = Queue.Front.Next;
+                    firstTemp.Next = null;
+                    return firstTemp;
+                }
+                Animal temp = Queue.Front;
+                Queue.Front = Queue.Front.Next;
+                tempQueue.Enqueue(temp.Value);
+            }
+            if (Queue.Front.Value == pref.Value)
+            {
+                Animal firstTemp = Queue.Front;
+                Queue.Front = Queue.Front.Next;
+                firstTemp.Next = null;
+                return firstTemp;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Challenges/AnimalShelter/AnimalShelter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StacksAndQueues.Classes;
+using AnimalShelter.Classes;
+
+namespace AnimalShelter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void FIFOAnimalShelter()
+        {
+
+        }
+    }
+}
