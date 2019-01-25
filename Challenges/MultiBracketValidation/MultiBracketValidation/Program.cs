9cee46dbb4bb010c25b92ce37ca7622fa0eab3d0
mbgoseco <mbgoseco@gmail.com>
2019-01-24 22:49:17 -0800

    stack and switch created

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using StacksAndQueues.Classes;
+
+namespace MultiBracketValidation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "([Hello{}( )[]world!])";
+            bool result = MultiBracketValidation(str);
+            Console.WriteLine($"Does \"{str}\" have balanced brackets? {result}");
+        }
+
+        public static bool MultiBracketValidation(string input)
+        {
+            Stack validator = new Stack();
+
+            char[] charString = input.ToCharArray();
+
+            for (int i = 0; i < charString.Length; i++)
+            {
+                switch (charString[i])
+                {
+                    case '(':
+                        validator.Push(charString[i]);
+                        break;
+                    case '[':
+                        validator.Push(charString[i]);
+                        break;
+                    case '{':
+                        validator.Push(charString[i]);
+                        break;
+                    case ')':
+                        if (i == 0) { return false; }   // If first char is a closing bracket then always false
+                        if (validator.Top.Value == charString[i])
+                        {
+                            validator.Pop();
+                        }
+                        break;
+                    case ']':
+                        if (i == 0) { return false; }
+                        if (validator.Top.Value == charString[i])
+                        {
+                            validator.Pop();
+                        }
+                        break;
+                    case '}':
+                        if (i == 0) { return false; }
+                        if (validator.Top.Value == charString[i])
+                        {
+                            validator.Pop();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Challenges/MultiBracketValidation/XUnitTestMBV/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using StacksAndQueues.Classes;
+using MultiBracketValidation;
+
+namespace XUnitTestMBV
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
