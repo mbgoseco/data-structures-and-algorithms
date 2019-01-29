850014d09943bfb067a7a7a8cb994b113d3ed6e4
mbgoseco <mbgoseco@gmail.com>
2019-01-28 22:20:45 -0800

    method working

---
+++ Challenges/FizzBuzzTree/XUnitTestFBT/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using FizzBuzzTree;
+using HappyTrees.Classes;
+
+namespace XUnitTestFBT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
