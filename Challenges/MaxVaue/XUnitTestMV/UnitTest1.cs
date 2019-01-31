31b8ec0ec6e3f0fa459f24b8a08dec0b168bfc55
mbgoseco <mbgoseco@gmail.com>
2019-01-31 01:18:31 -0800

    method complete

---
+++ Challenges/MaxVaue/XUnitTestMV/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using MaxVaue;
+using HappyTrees.Classes;
+
+namespace XUnitTestMV
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
