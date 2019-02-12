1a63e66559487dba708174ff9bdcfbc2a302be46
mbgoseco <mbgoseco@gmail.com>
2019-02-11 21:10:22 -0800

    method completed

---
+++ Challenges/UniqueStringCharacters/UniqueStringCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UniqueStringCharacters
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{AreCharactersUnique("ABC123abc")}");
+        }
+
+        public static bool AreCharactersUnique(string input)
+        {
+            for(int i = 0; i < input.Length; i++)
+            {
+                for(int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
