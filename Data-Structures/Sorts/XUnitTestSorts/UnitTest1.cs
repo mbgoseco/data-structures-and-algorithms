d8b982f0786eb857596ca21a4afff552864bc0fb
mbgoseco <mbgoseco@gmail.com>
2019-02-17 20:25:39 -0800

    Method created and working

---
+++ Data-Structures/Sorts/XUnitTestSorts/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Sorts;
+
+namespace XUnitTestSorts
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
