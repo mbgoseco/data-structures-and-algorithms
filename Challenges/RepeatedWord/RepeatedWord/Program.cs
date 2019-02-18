a349fa4e4241b6b0d65ba6c9e3b4d0fe6747bd46
mbgoseco <mbgoseco@gmail.com>
2019-02-18 13:45:07 -0800

    method successful using Split()

---
+++ Challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RepeatedWord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string yugeString = "How much wood could a, woodchuck chuck if, 'some' woodchuck; could chuck, wood? - William Shatner";
+
+            Console.WriteLine($"First repeated word is: {RepeatedWord(yugeString)}");
+        }
+
+        public static string RepeatedWord(string yugeString)
+        {
+            char[] delimiters = { ' ', ',', '.', '!', '?', ';', ':', '"', '\'', '\\', '/', '(', ')' };
+            string[] words = yugeString.Split(delimiters);
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                int j = i - 1;
+                string temp = words[i];
+
+                while (j >= 0)
+                {
+                    if (temp == words[j] && (temp != "" && words[j] != ""))
+                    {
+                        return temp;
+                    }
+
+                    j--;
+                }
+            }
+
+            return null;
+        }
+    }
+}
