e1d14471380ebd7b561eb78d7b267d49c2f0ba9e
mbgoseco <mbgoseco@gmail.com>
2019-02-19 23:53:39 -0800

    Tests created and passed

--- Challenges/TreeIntersection/TreeIntersection/Program.cs
+++ Challenges/TreeIntersection/TreeIntersection/Program.cs
@@ -9,27 +9,29 @@ namespace TreeIntersection
     {
         static void Main(string[] args)
         {
-            Node node = new Node(12);
-            node.Left = new Node(0);
-            node.Right = new Node(5);
-            node.Left.Left = new Node(13);
-            node.Left.Right = new Node(-4);
-            node.Right.Right = new Node(3);
-            node.Right.Right.Left = new Node(25);
+            BinaryTree treeOne = new BinaryTree();
+            treeOne.Root = new Node(12);
+            treeOne.Root.Left = new Node(0);
+            treeOne.Root.Right = new Node(5);
+            treeOne.Root.Left.Left = new Node(13);
+            treeOne.Root.Left.Right = new Node(-4);
+            treeOne.Root.Right.Right = new Node(3);
+            treeOne.Root.Right.Right.Left = new Node(25);
 
-            Node nodeTwo = new Node(7);
-            nodeTwo.Left = new Node(40);
-            nodeTwo.Right = new Node(13);   // Intersect
-            nodeTwo.Left.Left = new Node(22);
-            nodeTwo.Left.Right = new Node(-10); 
-            nodeTwo.Right.Right = new Node(3);  // Intersect
-            nodeTwo.Left.Right.Left = new Node(-4); // Intersect
-            nodeTwo.Right.Right.Left = new Node(111);
-            nodeTwo.Right.Right.Right = new Node(25);   // Intersect
+            BinaryTree treeTwo = new BinaryTree();
+            treeTwo.Root = new Node(7);
+            treeTwo.Root.Left = new Node(40);
+            treeTwo.Root.Right = new Node(13);   // Intersect
+            treeTwo.Root.Left.Left = new Node(22);
+            treeTwo.Root.Left.Right = new Node(-10); 
+            treeTwo.Root.Right.Right = new Node(3);  // Intersect
+            treeTwo.Root.Left.Right.Left = new Node(-4); // Intersect
+            treeTwo.Root.Right.Right.Left = new Node(111);
+            treeTwo.Root.Right.Right.Right = new Node(25);   // Intersect
 
             Console.WriteLine("Intersecting values of both trees are:");
 
-            var result = TreeIntersection(node, nodeTwo);
+            var result = TreeIntersection(treeOne, treeTwo);
 
             foreach (var i in result)
             {
@@ -44,13 +46,18 @@ namespace TreeIntersection
         /// <param name="rootOne">Root node of first binary tree</param>
         /// <param name="rootTwo">Root node of second binary tree</param>
         /// <returns>List of intersecting node values</returns>
-        public static object[] TreeIntersection(Node rootOne, Node rootTwo)
+        public static object[] TreeIntersection(BinaryTree rootOne, BinaryTree rootTwo)
         {
+            if (rootOne.Root == null || rootTwo.Root == null)
+            {
+                return null;
+            }
+
             List<object> intersectValues = new List<object>();
             HashTable table = new HashTable();
 
             Queue<Node> TreeQueueOne = new Queue<Node>();
-            TreeQueueOne.Enqueue(rootOne);
+            TreeQueueOne.Enqueue(rootOne.Root);
 
             while (TreeQueueOne.Count > 0)
             {
@@ -68,7 +75,7 @@ namespace TreeIntersection
             }
 
             Queue<Node> TreeQueueTwo = new Queue<Node>();
-            TreeQueueTwo.Enqueue(rootTwo);
+            TreeQueueTwo.Enqueue(rootTwo.Root);
 
             while (TreeQueueTwo.Count > 0)
             {
