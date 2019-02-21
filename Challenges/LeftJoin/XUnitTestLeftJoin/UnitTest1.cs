842134f02d032ffd8b5ee013489220e8d344d787
mbgoseco <mbgoseco@gmail.com>
2019-02-20 22:48:33 -0800

    Method finished and working

---
+++ Challenges/LeftJoin/XUnitTestLeftJoin/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using HashTables.Classes;
+using LeftJoin;
+
+namespace XUnitTestLeftJoin
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
