88e565f898b1c231f392f3bad67de8557d09ea37
mbgoseco <mbgoseco@gmail.com>
2019-02-27 02:41:44 -0800

    Method complete and operational

---
+++ Challenges/FindEdges/XUnitTestFindEdges/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using FindEdges;
+using Graphs.Classes;
+
+namespace XUnitTestFindEdges
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
